2af4d476b339a48c903b8427a3edffb1e2c49e1b
Reto Schneider <github@reto-schneider.ch>
2012-07-11 22:51:20 +0200

    Add latest news.

--- js/news.js
+++ js/news.js
@@ -1,64 +1,21 @@
-//Laden der News
-
+//Laden der aktuellsten SG-News
 function loadNews() {
-	//div-Tag mit id 'news-items' leeren
-	document.getElementById("news-items").innerHTML = "";
+	$news = $("#news-items");
+	// Nette Nachricht anzeigen...
+	$news.html('<p>Lade neuste Nachrichten...</p>');
+	// Feed-JSON-URL via JSONP lesen
+	var url = 'http://sg.piratenpartei.ch/?json=1&callback=?';
 
-	//Feed-XML lesen
-	xdr = new XDomainRequest();
-	
-	xdr.onload=function() {
-		
-		var items = xdr.responseXML.getElementsByTagName('item');		
-		
-		for (var i = 0; i <= 5; ++i) {
-			
-			var entryDate = new Date(items[i].getElementsByTagName('pubDate')[0].firstChild.nodeValue)
-			var pubDate = entryDate.toLocaleDateString() + ', ' + entryDate.toLocaleTimeString();
-			
-			document.getElementById("news-items").innerHTML = document.getElementById("news-items").innerHTML + 
-				"<h1>" + items[i].getElementsByTagName('title')[0].firstChild.nodeValue + "</h1>" +
-				"<div class='news-item-date'>" + pubDate + "</div>" + 
-				"<p>" + items[i].getElementsByTagName('content:encoded')[0].firstChild.nodeValue + "</p>"
-		}		
-	}
-	
-	xdr.open("GET", "http://sg.piratenpartei.ch/feed");
-	xdr.contentType = "text/plain";
-	xdr.send();
-	
-	return(true);
-}
-
-
-function loadNews_old() {
-	//div-Tag mit id 'news-items' leeren
-	document.getElementById("news-items").innerHTML = "";
-
-	//Feed-XML lesen
-	if (window.XMLHttpRequest) {
-		xhttp = new XMLHttpRequest();
-	} else { // IE 5/6
-		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  
-	xhttp.open("GET", "http://sg.piratenpartei.ch/feed", false);
-	xhttp.send();
-	
-	xmlDoc = xhttp.responseXML;
-	echo(xmlDoc);
-	var items = xmlDoc.getElementsByTagName('item');
-	
-	for (var i = 0; i <= 5; ++i) {
-		
-		var entryDate = new Date(items[i].getElementsByTagName('pubDate')[0].firstChild.nodeValue)
-		var pubDate = entryDate.toLocaleDateString() + ', ' + entryDate.toLocaleTimeString();
-		
-		document.getElementById("news-items").innerHTML = document.getElementById("news-items").innerHTML + 
-			"<h1>" + items[i].getElementsByTagName('title')[0].firstChild.nodeValue + "</h1>" +
-			"<div class='news-item-date'>" + pubDate + "</div>" + 
-			"<p>" + items[i].getElementsByTagName('content:encoded')[0].firstChild.nodeValue + "</p>"
-	}
-	
-	return(true);
+	$.getJSON(url, function(postings) {
+		// div-Tag mit id 'news-items' leeren
+		$news.empty();
+		postings.posts.forEach(function(post) {
+			var linkMore = '<a target="_blank" href="' + post.url
+					+ '">[...]</a>';
+			var shortText = post.excerpt.replace(/.{5}$/, linkMore);
+			$news.append('<h1>' + post.title + '</h1>');
+			$news.append('<b>' + post.date.match(/.{10}/) + '</b>: '
+					+ shortText);
+		});
+	});
 }
